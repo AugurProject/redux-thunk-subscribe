131daed94c8ca47c7d1b62e43068a50ce8b3d782
Jack Peterson <jack@tinybike.net>
2017-04-17 13:12:06 -0700

    Fixed extra argument

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 "use strict";
 
-var createThunkSubscribeEnhancer = function () {
+var createThunkSubscribeEnhancer = function (extraArgument) {
   return function (createStore) {
     return function (reducer, initialState) {
       var store = createStore(reducer, initialState);
       var dispatch = store.dispatch;
       store.dispatch = function (action) {
         if (typeof action === "function") {
-          return action(store.dispatch, store.getState, store.subscribe);
+          return action(store.dispatch, store.getState, store.subscribe, extraArgument);
         }
         return dispatch(action);
       };
--- test/index.js
+++ test/index.js
@@ -1,16 +1,19 @@
 "use strict";
 
 var test = require("tape");
-var redux = require("redux");
+var createStore = require("redux").createStore;
 var thunkSubscribeEnhancer = require("../src");
+
+var reducer = function (state) { return state; };
+var happyDaysAreHereAgain = { type: "HAPPY_DAYS_ARE_HERE_AGAIN" };
 
 test("redux-thunk-subscribe", function (t) {
   var enhancedCreateStore, store;
   t.assert(typeof thunkSubscribeEnhancer === "function", "thunkSubscribeEnhancer should be a function");
   t.assert(typeof thunkSubscribeEnhancer.withExtraArgument === "function", "thunkSubscribeEnhancer.withExtraArgument should be a function");
-  enhancedCreateStore = thunkSubscribeEnhancer(redux.createStore);
+  enhancedCreateStore = thunkSubscribeEnhancer(createStore);
   t.assert(typeof enhancedCreateStore === "function", "thunkSubscribeEnhancer(createStore) should be a function");
-  store = enhancedCreateStore(function (state) { return state; }, {});
+  store = enhancedCreateStore(reducer, {});
   t.assert(typeof store.getState === "function", "store.getState should be a function");
   t.assert(typeof store.dispatch === "function", "store.dispatch should be a function");
   t.assert(typeof store.subscribe === "function", "store.subscribe should be a function");
@@ -23,9 +26,28 @@ test("redux-thunk-subscribe", function (t) {
       t.strictEqual(dispatch, store.dispatch, "dispatch should be identical to store.dispatch");
       t.strictEqual(getState, store.getState, "getState should be identical to store.getState");
       t.strictEqual(subscribe, store.subscribe, "subscribe should be identical to store.subscribe");
-      t.deepEqual(dispatch({ type: "HAPPY_DAYS_ARE_HERE_AGAIN" }), { type: "HAPPY_DAYS_ARE_HERE_AGAIN" }, "happy days should be here again");
-      t.end();
+      t.strictEqual(dispatch(happyDaysAreHereAgain), happyDaysAreHereAgain, "happy days should be here again");
     };
   }
   store.dispatch(cueAction());
+  t.end();
+});
+
+test("with extra argument", function (t) {
+  var extraArgument = { isArgumentative: true };
+  var store = thunkSubscribeEnhancer.withExtraArgument(extraArgument)(createStore)(reducer, {});
+  function cueAction() {
+    return function (dispatch, getState, subscribe, extras) {
+      t.assert(typeof dispatch === "function", "dispatch should be a function inside a thunk");
+      t.assert(typeof getState === "function", "getState should be a function inside a thunk");
+      t.assert(typeof subscribe === "function", "subscribe should be a function inside a thunk");
+      t.strictEqual(extras, extraArgument, "extra argument should be available inside the thunk");
+      t.strictEqual(dispatch, store.dispatch, "dispatch should be identical to store.dispatch");
+      t.strictEqual(getState, store.getState, "getState should be identical to store.getState");
+      t.strictEqual(subscribe, store.subscribe, "subscribe should be identical to store.subscribe");
+      t.strictEqual(dispatch(happyDaysAreHereAgain), happyDaysAreHereAgain, "happy days should be here again");
+    };
+  }
+  store.dispatch(cueAction());
+  t.end();
 });
